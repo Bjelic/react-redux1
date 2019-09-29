82773bca76007cfeb48fc4268f537f3686da349b
Bjelic <bojanbjelic10@gmail.com>
2019-09-29 14:49:18 +0200

    Add files via upload

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator/check');
 
 const User = require('../../models/User');
@@ -61,18 +63,25 @@ router.post(
 
 			// Return jsonwebtoken
 
-			res.send('User registred');
+			const payload = {
+				user: {
+					id: user.id
+				}
+			};
+
+			jwt.sign(
+				payload,
+				config.get('jwtToken'),
+				{ expiresIn: 360000 },
+				(error, token) => {
+					if (error) throw error;
+					res.json({ token });
+				}
+			);
 		} catch (error) {
 			console.log(error.message);
 			res.status(500).send('Server error');
 		}
-		// See if user exist
-
-		// Get user gravatar
-
-		// Encrypt password
-
-		// Return jsonwebtoken
 
 		res.send('User route');
 	}
